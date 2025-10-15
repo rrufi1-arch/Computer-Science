818a30139d1c0ad2445336200fc47f38be5cc44b
rrufi1-arch <rrufi1@hwemail.com>
2025-10-14 17:34:36 -0700

    completed NameOps lab

--- 1.5 NameOps/NameOps.java
+++ 1.5 NameOps/NameOps.java
@@ -8,10 +8,51 @@ public class NameOps {
     }
 
     public static int indexOfFirstSpace(String name) {
-        for (int letter = 0; letter <= name.length() ;letter++) {
-            if () {
+        return name.indexOf(" ");
+    }
 
+    public static int indexOfSecondSpace(String name) {
+        return name.indexOf(" ", indexOfFirstSpace(name) + 1);
+    }
+
+    public static String findFirstName(String name) {
+        if (name.indexOf(" ") != -1) {
+            return name.substring(0, name.indexOf(" "));
+        } else {
+            return name;
+        }
+    }
+
+    public static String findLastName(String name) {
+        if (name.lastIndexOf(" ") != -1) {
+            return name.substring(name.lastIndexOf(" "),name.length());
+        } else {
+            return "";
+        }
+    }
+
+    public static String findMiddleName(String name) {
+        if (name.indexOf(" ") != -1) {
+            if (name.lastIndexOf(" ") != name.indexOf(" ")) {
+                return name.substring(name.indexOf(" ") - 1, name.lastIndexOf(" "));
+            } else {
+                return "";
             }
+        } else {
+            return "";
+        }
+    }
+
+    public static String generateLastFirstMidInitial(String name) {
+        if (name.indexOf(" ") != -1) {
+            if (name.lastIndexOf(" ") != name.indexOf(" ")) {
+                return "\"" + findLastName(name) + ", " + findFirstName(name) + 
+                    " " + findMiddleName(name).charAt(0) + ".\"";
+            } else {
+                return "\"" + findLastName(name) + ", " + findFirstName(name) + ".\"";
+            }
+        } else {
+            return "\""+name+"\"";
         }
     }
 }
