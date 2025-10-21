fa6992041da3666fce8c1262ce33928063fac0b5
rrufi1-arch <rrufi1@hwemail.com>
2025-10-21 10:59:11 -0700

    small adjustments to Triangle Loops Lab

---
+++ Lab 1.6 TriangleLoops/TriangleLoopsTester.java
@@ -0,0 +1,12 @@
+public class TriangleLoopsTester {
+    public static void main(String[] args) {
+        System.out.println(TriangleLoops.createLetterTriangleUp(8, 'A'));
+        System.out.println(TriangleLoops.createLetterTriangleUp(4, 'Z'));
+        System.out.println(TriangleLoops.createLetterTriangleDown(11, 'R'));
+        System.out.println(TriangleLoops.createLetterTriangleDown(7, 'C'));
+        System.out.println(TriangleLoops.createNumbersTriangle(6));
+        System.out.println(TriangleLoops.createNumbersTriangle(17));
+        System.out.println(TriangleLoops.createAlphabetTriangle(3));
+        System.out.println(TriangleLoops.createAlphabetTriangle(216));
+    }
+}
